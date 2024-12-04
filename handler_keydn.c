60fcd7d85dbd5da99e4c495d32e99b26006e0c06
ander-vieira <ander.vieira741@gmail.com>
2024-12-04 23:41:45 +0100

    Improve rendering

--- handler_keydn.c
+++ handler_keydn.c
@@ -6,7 +6,7 @@
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 16:29:47 by andeviei          #+#    #+#             */
-/*   Updated: 2024/12/04 19:38:11 by andeviei         ###   ########.fr       */
+/*   Updated: 2024/12/04 23:09:49 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,37 +22,41 @@ int	handler_keydn(int keycode, t_runwn *runwn)
 		end_program(runwn);
 	else if (keycode == KEY_W)
 	{
-		printf("PRESSED W\n");
 		runwn->cubed->pos.x += runwn->cubed->dir.x * 0.2;
 		runwn->cubed->pos.y += runwn->cubed->dir.y * 0.2;
-		printf("BBB %f %f\n", runwn->cubed->pos.x, runwn->cubed->pos.y);
+		printf("New position: %f %f\n", runwn->cubed->pos.x, runwn->cubed->pos.y);
 	}
 	else if (keycode == KEY_S)
 	{
-		printf("PRESSED S\n");
 		runwn->cubed->pos.x -= runwn->cubed->dir.x * 0.2;
 		runwn->cubed->pos.y -= runwn->cubed->dir.y * 0.2;
-		printf("BBB %f %f\n", runwn->cubed->pos.x, runwn->cubed->pos.y);
+		printf("New position: %f %f\n", runwn->cubed->pos.x, runwn->cubed->pos.y);
 	}
 	else if (keycode == KEY_A)
-		printf("PRESSED A\n");
+	{
+		runwn->cubed->pos.x += runwn->cubed->dir.y * 0.2;
+		runwn->cubed->pos.y -= runwn->cubed->dir.x * 0.2;
+		printf("New position: %f %f\n", runwn->cubed->pos.x, runwn->cubed->pos.y);
+	}
 	else if (keycode == KEY_D)
-		printf("PRESSED D\n");
+	{
+		runwn->cubed->pos.x -= runwn->cubed->dir.y * 0.2;
+		runwn->cubed->pos.y += runwn->cubed->dir.x * 0.2;
+		printf("New position: %f %f\n", runwn->cubed->pos.x, runwn->cubed->pos.y);
+	}
 	else if (keycode == KEY_LEFT)
 	{
-		printf("PRESSED LEFT\n");
-		dir.x = runwn->cubed->dir.x * 0.8 + runwn->cubed->dir.y * 0.6;
-		dir.y = runwn->cubed->dir.y * 0.8 - runwn->cubed->dir.x * 0.6;
+		dir.x = runwn->cubed->dir.x * 0.98480775301 + runwn->cubed->dir.y * 0.17364817766;
+		dir.y = runwn->cubed->dir.y * 0.98480775301 - runwn->cubed->dir.x * 0.17364817766;
 		runwn->cubed->dir = dir;
-		printf("AAA %f %f\n", runwn->cubed->dir.x, runwn->cubed->dir.y);
+		printf("New direction: %f %f\n", runwn->cubed->dir.x, runwn->cubed->dir.y);
 	}
 	else if (keycode == KEY_RIGHT)
 	{
-		printf("PRESSED RIGHT\n");
-		dir.x = runwn->cubed->dir.x * 0.8 - runwn->cubed->dir.y * 0.6;
-		dir.y = runwn->cubed->dir.x * 0.6 + runwn->cubed->dir.y * 0.8;
+		dir.x = runwn->cubed->dir.x * 0.98480775301 - runwn->cubed->dir.y * 0.17364817766;
+		dir.y = runwn->cubed->dir.y * 0.98480775301 + runwn->cubed->dir.x * 0.17364817766;
 		runwn->cubed->dir = dir;
-		printf("AAA %f %f\n", runwn->cubed->dir.x, runwn->cubed->dir.y);
+		printf("New direction: %f %f\n", runwn->cubed->dir.x, runwn->cubed->dir.y);
 	}
 	return (0);
 }
--- handler_loop.c
+++ handler_loop.c
@@ -6,69 +6,12 @@
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 22:58:27 by andeviei          #+#    #+#             */
-/*   Updated: 2024/12/04 19:36:15 by andeviei         ###   ########.fr       */
+/*   Updated: 2024/12/04 23:41:12 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubed.h"
 
-// static void	get_pos(t_runwn *runwn)
-// {
-// 	double	t_x;
-// 	double	t_y;
-// 	double	d_x;
-// 	double	d_y;
-// 	t_vec2	pos;
-// 	t_vec2	dir;
-
-// 	pos = runwn->cubed->pos;
-// 	dir = runwn->cubed->dir;
-// 	if (dir.x > 0)
-// 	{
-// 		d_x = 1 / dir.x;
-// 		t_x = d_x * (1 - fmod(pos.x, 1.));
-// 	}
-// 	else if (dir.x < 0)
-// 	{
-// 		d_x = 1 / (-dir.x);
-// 		t_x = d_x * fmod(pos.x, 1.);
-// 	}
-// 	if (dir.y > 0)
-// 	{
-// 		d_y = 1 / dir.y;
-// 		t_y = d_y * (1 - fmod(pos.y, 1.));
-// 	}
-// 	else if (dir.y < 0)
-// 	{
-// 		d_y = 1 / (-dir.y);
-// 		t_y = d_y * fmod(pos.y, 1.);
-// 	}
-// 	while (1)
-// 	{
-// 		if (dir.y == 0 || (dir.x != 0 && t_x < t_y))
-// 		{
-// 			t_y -= t_x;
-// 			t_x = 1 / fabs(dir.x); 
-// 		}
-// 		else
-// 		{
-// 		}
-// 	}
-// }
-
-static t_vec2	get_col_dir(t_runwn *r, t_uint x)
-{
-	double	slope;
-	double	total;
-	double	scale = 1;
-
-	slope = scale * (2 * (double)x / WIN_W - 1);
-	total = sqrt(slope*slope + 1);
-	return (vec2_init(r->cubed->dir.x / total - r->cubed->dir.y * slope / total,
-		r->cubed->dir.y / total + r->cubed->dir.x * slope / total));
-}
-
-#include <stdio.h>
 static t_uint	get_height(t_runwn *runwn, t_uint x)
 {
 	t_vec2	dir;
@@ -76,61 +19,63 @@ static t_uint	get_height(t_runwn *runwn, t_uint x)
 	double	dx;
 	double	y0;
 	double	dy;
+	double	slope;
+	double	total;
+	t_ivec2	tile;
 
-	dir = get_col_dir(runwn, x);
+	slope = 2 * (double)x / WIN_W - 1;
+	total = sqrt(slope*slope + 1);
+	dir = vec2_init(runwn->cubed->dir.x / total - runwn->cubed->dir.y * slope / total,
+		runwn->cubed->dir.y / total + runwn->cubed->dir.x * slope / total);
 	if (dir.x < 0)
+	{
+		tile.x = floor(runwn->cubed->pos.x);
 		x0 = (floor(runwn->cubed->pos.x) - runwn->cubed->pos.x) / dir.x;
+		dx = -1 / dir.x;
+	}
 	else
+	{
+		tile.x = floor(runwn->cubed->pos.x);
 		x0 = (ceil(runwn->cubed->pos.x) - runwn->cubed->pos.x) / dir.x;
-	dx = 1 / fabs(dir.x);
+		dx = 1 / dir.x;
+	}
 	if (dir.y < 0)
+	{
+		tile.y = floor(runwn->cubed->pos.y);
 		y0 = (floor(runwn->cubed->pos.y) - runwn->cubed->pos.y) / dir.y;
+		dy = -1 / dir.y;
+	}
 	else
+	{
+		tile.y = floor(runwn->cubed->pos.y);
 		y0 = (ceil(runwn->cubed->pos.y) - runwn->cubed->pos.y) / dir.y;
-	dy = 1 / fabs(dir.y);
-	printf("CASTING RAY (%f, %f)\n", dir.x, dir.y);
+		dy = 1 / dir.y;
+	}
 	while (TRUE)
 	{
 		t_vec2 v1;
-		t_ivec2 v2;
 		if (y0 < x0)
 		{
+			if (dir.y > 0)
+				tile.y += 1;
+			else
+				tile.y -= 1;
 			v1.x = runwn->cubed->pos.x + y0 * dir.x;
 			v1.y = runwn->cubed->pos.y + y0 * dir.y;
-			if (dir.y > 0)
-			{
-				v2 = ivec2_new(floor(v1.x), round(v1.y));
-			}
-			else
-			{
-				v2 = ivec2_new(floor(v1.x), round(v1.y) - 1);
-			}
-			if (map_get(&(runwn->cubed->map), v2.x, v2.y) == '1')
-			{
-				printf("RAY COLLIDED WITH WALL AT POSITION (%f, %f) (%zu, %zu)\n", v1.x, v1.y, v2.x, v2.y);
-				return (100 / y0);
-			}
-			// printf("RAY GOES THROUGH POSITION (%f, %f)\n", v1.x, v1.y);
+			if (map_get(&(runwn->cubed->map), tile.x, tile.y) == '1')
+				return (round(100. * total / y0));
 			y0 += dy;
 		}
 		else
 		{
+			if (dir.x > 0)
+				tile.x += 1;
+			else
+				tile.x -= 1;
 			v1.x = runwn->cubed->pos.x + x0 * dir.x;
 			v1.y = runwn->cubed->pos.y + x0 * dir.y;
-			if (dir.x > 0)
-			{
-				v2 = ivec2_new(round(v1.x), floor(v1.y));
-			}
-			else
-			{
-				v2 = ivec2_new(round(v1.x) - 1, floor(v1.y));
-			}
-			if (map_get(&(runwn->cubed->map), v2.x, v2.y) == '1')
-			{
-				printf("RAY COLLIDED WITH WALL AT POSITION (%f, %f) (%zu, %zu)\n", v1.x, v1.y, v2.x, v2.y);
-				return (100 / x0);
-			}
-			// printf("RAY GOES THROUGH POSITION (%f, %f)\n", v1.x, v1.y);
+			if (map_get(&(runwn->cubed->map), tile.x, tile.y) == '1')
+				return (round(100. * total / x0));
 			x0 += dx;
 		}
 		if (v1.x < 0 || v1.x >= runwn->cubed->map.w || v1.y < 0 || v1.y >= runwn->cubed->map.h)
@@ -148,6 +93,8 @@ static void	draw_view(t_runwn *runwn)
 	while (col.x < WIN_W)
 	{
 		col.y = get_height(runwn, col.x);
+		if (col.y > WIN_H / 2)
+			col.y = WIN_H / 2;
 		img_fill(runwn->scr, 0x00000000, ivec2_new(col.x, WIN_H / 2 - col.y), ivec2_new(1, 2 * col.y));
 		col.x++;
 	}
