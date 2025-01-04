2bb610087d15bb9a1b5ffecc8d5fa11ed8004402
alex <alexhigueramartinez@gmail.com>
2025-01-04 21:12:43 +0100

    add atoi

--- str.c
+++ str.c
@@ -6,7 +6,7 @@
 /*   By: alex <ahiguera@student.42urduliz.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 19:27:38 by alex              #+#    #+#             */
-/*   Updated: 2025/01/04 19:46:38 by alex             ###   ########.fr       */
+/*   Updated: 2025/01/04 20:20:31 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,4 +91,30 @@ char	*ft_strdup(const char *s1)
 	ft_memcpy(str, s1, len);
 	str[len] = '\0';
 	return (str);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	rest;
+
+	sign = 1;
+	rest = 0;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		rest = rest * 10 + (str[i] - '0');
+		i++;
+	}
+	return (rest * sign);
 }
