2f9d6a5fcba805adbcb34c2bcdd2c237674d7005
alex <alexhigueramartinez@gmail.com>
2025-01-04 19:49:45 +0100

    add auxiliary functions

--- str.c
+++ str.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   str.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alex <ahiguera@student.42urduliz.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 19:27:38 by alex              #+#    #+#             */
-/*   Updated: 2025/01/02 20:37:26 by andeviei         ###   ########.fr       */
+/*   Updated: 2025/01/04 19:46:38 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubed.h"
 
-size_t	str_len(t_str str) {
+size_t	str_len(t_str str)
+{
 	size_t	len;
 
 	if (str == NULL)
@@ -33,4 +34,61 @@ void	str_copy(t_str dest, t_str src, size_t len)
 		dest[i] = src[i];
 		i++;
 	}
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+	char	*origin;
+	char	*dest;
+
+	if (dst == NULL && src == NULL && n != 0)
+		return (NULL);
+	i = 0;
+	origin = (char *)src;
+	dest = (char *)dst;
+	while (i < n)
+	{
+		dest[i] = origin[i];
+		i++;
+	}
+	return (dst);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if ((unsigned char) s1[i] != (unsigned char) s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_memcpy(str, s1, len);
+	str[len] = '\0';
+	return (str);
 }
